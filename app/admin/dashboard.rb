ed6cefa65f5403bfab4cb4606769a6a9c136dc21
Sirajus Salayhin <salayhin@gmail.com>
2015-04-08 01:40:14 +0600

    - Dashboard recent post added

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -1,13 +1,30 @@
-ActiveAdmin.register_page "Dashboard" do
+ActiveAdmin.register_page 'Dashboard' do
 
-  menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
+  menu false
 
-  content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+  content title: proc{ I18n.t('active_admin.dashboard') } do
+    # div class: "blank_slate_container", id: "dashboard_default_message" do
+    #   span class: "blank_slate" do
+    #     span I18n.t("active_admin.dashboard_welcome.welcome")
+    #   end
+    # end
+
+    columns do
+      column do
+        panel 'Recent Posts' do
+          table_for Post.where(:is_published => true).order('id desc').limit(10) do
+            column('Title')   {|post| link_to(post.title, admin_post_path(post))}
+          end
+        end
       end
+      #
+      # column do
+      #   panel 'Recent Customers' do
+      #     table_for User.order('id desc').limit(10).each do |customer|
+      #       column(:email)    {|customer| link_to(customer.email, admin_customer_path(customer)) }
+      #     end
+      #   end
+      # end
     end
 
     # Here is an example of a simple dashboard with columns and panels.
