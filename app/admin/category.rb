95a9047fd12efa2f3d577e892c790ee176d03314
Sirajus Salayhin <salayhin@gmail.com>
2015-04-08 00:46:45 +0600

    - Admin Category added

--- app/admin/category.rb
+++ app/admin/category.rb
@@ -1,18 +1,35 @@
 ActiveAdmin.register Category do
 
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  permit_params :name
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:permitted, :attributes]
-  #   permitted << :other if resource.something?
-  #   permitted
-  # end
+  #menu false
 
+  permit_params :name, :slug
+
+  index do
+    selectable_column
+    column auto_link :name
+    column :created_at
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :name
+      row :created_at
+    end
+  end
+
+  # Product Form
+  form :html => { :enctype => "multipart/form-data" } do |f|
+    f.semantic_errors *f.object.errors.keys
+    f.inputs do
+      f.input :name
+      f.actions
+    end
+  end
+
+
+  ####<$ filter options $>####
+  filter :name, as: :string
 
 end
